6c56d239efb7199a81cb72997d0aeeecf3f99ada
AviCedCoss <avinashsrivastva@cedcommerce.com>
2023-09-24 16:56:04 +0530

    Changes

--- app/src/main/java/com/kissmiss/videocallingapp/activities/ConnectingActivity.java
+++ app/src/main/java/com/kissmiss/videocallingapp/activities/ConnectingActivity.java
@@ -38,67 +38,45 @@ public class ConnectingActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
 
         String profile = getIntent().getStringExtra("profile");
-        Glide.with(this)
-                .load(profile)
-                .into(binding.profile);
+        Glide.with(this).load(profile).into(binding.profile);
 
         String username = mAuth.getUid();
 
-        database.getReference().child("users")
-                .orderByChild("status")
-                .equalTo(0).limitToFirst(1)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                        if (snapshot.getChildrenCount() > 0) {
-                            isOkay = true;
-                            // Room Available
-                            for (DataSnapshot childSnap : snapshot.getChildren()) {
-                                Random myRand = new Random();
-                                if (myRand.nextBoolean()) {
-                                    database.getReference()
-                                            .child("users")
-                                            .child(childSnap.getKey())
-                                            .child("incoming")
-                                            .setValue(username);
-                                    database.getReference()
-                                            .child("users")
-                                            .child(childSnap.getKey())
-                                            .child("status")
-                                            .setValue(1);
-                                    Intent intent = new Intent(ConnectingActivity.this, FakeVideoActivity.class);
-                                    String incoming = childSnap.child("incoming").getValue(String.class);
-                                    String createdBy = childSnap.child("createdBy").getValue(String.class);
-                                    boolean isAvailable = childSnap.child("isAvailable").getValue(Boolean.class);
-                                    intent.putExtra("username", username);
-                                    intent.putExtra("incoming", incoming);
-                                    intent.putExtra("createdBy", createdBy);
-                                    intent.putExtra("isAvailable", isAvailable);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                                else {
-                                    database.getReference()
-                                            .child("users")
-                                            .child(childSnap.getKey())
-                                            .child("incoming")
-                                            .setValue(username);
-                                    database.getReference()
-                                            .child("users")
-                                            .child(childSnap.getKey())
-                                            .child("status")
-                                            .setValue(1);
-                                    Intent intent = new Intent(ConnectingActivity.this, CallActivity.class);
-                                    String incoming = childSnap.child("incoming").getValue(String.class);
-                                    String createdBy = childSnap.child("createdBy").getValue(String.class);
-                                    boolean isAvailable = childSnap.child("isAvailable").getValue(Boolean.class);
-                                    intent.putExtra("username", username);
-                                    intent.putExtra("incoming", incoming);
-                                    intent.putExtra("createdBy", createdBy);
-                                    intent.putExtra("isAvailable", isAvailable);
-                                    startActivity(intent);
-                                    finish();
-                                }
+        database.getReference().child("users").orderByChild("status").equalTo(0).limitToFirst(1).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if (snapshot.getChildrenCount() > 0) {
+                    isOkay = true;
+                    // Room Available
+                    for (DataSnapshot childSnap : snapshot.getChildren()) {
+                        Random myRand = new Random();
+                        if (myRand.nextBoolean()) {
+                            database.getReference().child("users").child(childSnap.getKey()).child("incoming").setValue(username);
+                            database.getReference().child("users").child(childSnap.getKey()).child("status").setValue(1);
+                            Intent intent = new Intent(ConnectingActivity.this, FakeVideoActivity.class);
+                            String incoming = childSnap.child("incoming").getValue(String.class);
+                            String createdBy = childSnap.child("createdBy").getValue(String.class);
+                            boolean isAvailable = childSnap.child("isAvailable").getValue(Boolean.class);
+                            intent.putExtra("username", username);
+                            intent.putExtra("incoming", incoming);
+                            intent.putExtra("createdBy", createdBy);
+                            intent.putExtra("isAvailable", isAvailable);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            database.getReference().child("users").child(childSnap.getKey()).child("incoming").setValue(username);
+                            database.getReference().child("users").child(childSnap.getKey()).child("status").setValue(1);
+                            Intent intent = new Intent(ConnectingActivity.this, CallActivity.class);
+                            String incoming = childSnap.child("incoming").getValue(String.class);
+                            String createdBy = childSnap.child("createdBy").getValue(String.class);
+                            boolean isAvailable = childSnap.child("isAvailable").getValue(Boolean.class);
+                            intent.putExtra("username", username);
+                            intent.putExtra("incoming", incoming);
+                            intent.putExtra("createdBy", createdBy);
+                            intent.putExtra("isAvailable", isAvailable);
+                            startActivity(intent);
+                            finish();
+                        }
                               /*  Intent intent = new Intent(ConnectingActivity.this, CallActivity.class);
                                 String incoming = childSnap.child("incoming").getValue(String.class);
                                 String createdBy = childSnap.child("createdBy").getValue(String.class);
@@ -109,42 +87,36 @@ public class ConnectingActivity extends AppCompatActivity {
                                 intent.putExtra("isAvailable", isAvailable);
                                 startActivity(intent);
                                 finish();*/
-                            }
-                        } else {
-                            // Not Available
-                            HashMap<String, Object> room = new HashMap<>();
-                            room.put("incoming", username);
-                            room.put("createdBy", username);
-                            room.put("isAvailable", true);
-                            room.put("status", 0);
+                    }
+                } else {
+                    // Not Available
+                    HashMap<String, Object> room = new HashMap<>();
+                    room.put("incoming", username);
+                    room.put("createdBy", username);
+                    room.put("isAvailable", true);
+                    room.put("status", 0);
 
-                            database.getReference()
-                                    .child("users")
-                                    .child(username)
-                                    .setValue(room).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            database.getReference()
-                                                    .child("users")
-                                                    .child(username).addValueEventListener(new ValueEventListener() {
-                                                        @Override
-                                                        public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                                                            if (snapshot.child("status").exists()) {
-                                                                if (snapshot.child("status").getValue(Integer.class) == 1) {
-                                                                    if (isOkay)
-                                                                        return;
-                                                                    isOkay = true;
-                                                                    Intent intent = new Intent(ConnectingActivity.this, CallActivity.class);
-                                                                    String incoming = snapshot.child("incoming").getValue(String.class);
-                                                                    String createdBy = snapshot.child("createdBy").getValue(String.class);
-                                                                    boolean isAvailable = snapshot.child("isAvailable").getValue(Boolean.class);
-                                                                    intent.putExtra("username", username);
-                                                                    intent.putExtra("incoming", incoming);
-                                                                    intent.putExtra("createdBy", createdBy);
-                                                                    intent.putExtra("isAvailable", isAvailable);
-                                                                    startActivity(intent);
-                                                                    finish();
-                                                                }
+                    database.getReference().child("users").child(username).setValue(room).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            database.getReference().child("users").child(username).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                                    if (snapshot.child("status").exists()) {
+                                        if (snapshot.child("status").getValue(Integer.class) == 1) {
+                                            if (isOkay) return;
+                                            isOkay = true;
+                                            Intent intent = new Intent(ConnectingActivity.this, CallActivity.class);
+                                            String incoming = snapshot.child("incoming").getValue(String.class);
+                                            String createdBy = snapshot.child("createdBy").getValue(String.class);
+                                            boolean isAvailable = snapshot.child("isAvailable").getValue(Boolean.class);
+                                            intent.putExtra("username", username);
+                                            intent.putExtra("incoming", incoming);
+                                            intent.putExtra("createdBy", createdBy);
+                                            intent.putExtra("isAvailable", isAvailable);
+                                            startActivity(intent);
+                                            finish();
+                                        }
                                                               /*  if (snapshot.child("status").getValue(Integer.class) == 0) {
                                                                     Intent intent = new Intent(ConnectingActivity.this, FakeVideoActivity.class);
                                                                     String incoming = snapshot.child("incoming").getValue(String.class);
@@ -157,24 +129,24 @@ public class ConnectingActivity extends AppCompatActivity {
                                                                     startActivity(intent);
                                                                     finish();
                                                                 }*/
-                                                            }
-                                                        }
+                                    }
+                                }
 
-                                                        @Override
-                                                        public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                                @Override
+                                public void onCancelled(@NonNull @NotNull DatabaseError error) {
 
-                                                        }
-                                                    });
-                                        }
-                                    });
-
+                                }
+                            });
                         }
-                    }
+                    });
 
-                    @Override
-                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                }
+            }
 
-                    }
-                });
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 }
